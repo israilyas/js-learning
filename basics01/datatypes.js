a5fa42046084e47d5c8f56e9e20441485b7344f4
Isra Ilyas <israilyasq@gmail.com>
2024-08-06 18:29:40 +0500

    studied datatype

---
+++ basics01/datatypes.js
@@ -0,0 +1,22 @@
+/* Data Types in JS 
+1) Primitive Datatypes
+    -> String  => ""
+    -> Number    (range => 2 to the power 53)
+    -> bigint
+    -> Bolean  (true or false)
+    -> Null  => standalon value(presentation of empty value-we can assign null to any variable)
+    -> Undefined => value not defined only
+    ->Symbol => unique
+2) Non-Primitive Datatypes    
+    -> Object
+    -> Array
+*/
+
+//"use strict"; //treat all JS code as newer version
+// alert(3+3) we are using nodejs not browser so canot use alert here
+
+let a = 'hello'
+
+console.log(typeof 56)
+console.log(typeof null) //object
+console.log(typeof undefined)// undefined
