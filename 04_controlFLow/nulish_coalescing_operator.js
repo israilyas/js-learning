8278ecd688e1e2ad04e9ac08945d094deb0a2fcf
Isra Ilyas <israilyasq@gmail.com>
2024-08-09 23:45:00 +0500

    studied control flow in js , if else, nested if else, switch case, truthy and falsy values, nulish coalescing operator and turnary operator

---
+++ 04_controlFLow/nulish_coalescing_operator.js
@@ -0,0 +1,16 @@
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 15
+// val1 = undefined ?? 20
+val1 = undefined ?? 25 ?? 30
+
+console.log(val1);
+
+//  TERNARY OPERATOR
+// condition ?  true :  false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log('less than 80'): console.log('More than 80')
+
+
